6fe3143af08c6550dc1e595ddcea916390427608
bomber <bomber@70.70.169.217>
2017-06-16 15:34:06 +0900

    ZoneId 추가

--- src/test/java/org/codelabor/example/time/LocalDateTest.java
+++ src/test/java/org/codelabor/example/time/LocalDateTest.java
@@ -1,6 +1,8 @@
 package org.codelabor.example.time;
 
 import java.time.LocalDate;
+import java.time.ZoneId;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.junit.Test;
@@ -11,6 +13,7 @@ public class LocalDateTest {
 	@Test
 	public void testNow() {
 		logger.debug("now: {}", LocalDate.now());
+		logger.debug("now: {}, system default zone id: {}", LocalDate.now(ZoneId.systemDefault()), ZoneId.systemDefault());
 	}
 
 }
