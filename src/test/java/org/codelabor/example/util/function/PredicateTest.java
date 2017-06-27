ca7dbedcdd6aac02c2d38757bcb8f1e03630adde
codelabor <codelabor@gmail.com>
2017-06-27 15:36:57 +0900

    테스트 케이스 보완

--- src/test/java/org/codelabor/example/util/function/PredicateTest.java
+++ src/test/java/org/codelabor/example/util/function/PredicateTest.java
@@ -6,12 +6,22 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class PredicateTest {
+	
+	Predicate<String> isBiggerThan5 = (a) -> a.length() > 5;
+	Predicate<String> startsWith1 = (a) -> a.startsWith("1");
 
 	@Test
 	public void predicateTest() {
-		Predicate<String> predicate1 = (a) -> a.length() > 5;
-		Assert.assertFalse(predicate1.test("12345"));
-		Assert.assertTrue(predicate1.test("123456"));
+		
+		Assert.assertTrue(isBiggerThan5.test("123456"));
+		Assert.assertFalse(isBiggerThan5.test("12345"));
+		
+		Assert.assertTrue(startsWith1.test("12345"));
+		Assert.assertFalse(startsWith1.test("2345"));
+		
+		Assert.assertTrue(isBiggerThan5.and(startsWith1).test("123456"));
+		Assert.assertTrue(isBiggerThan5.or(startsWith1).test("12345"));
+		Assert.assertTrue(isBiggerThan5.negate().test("2345"));
 	}
 
 }
